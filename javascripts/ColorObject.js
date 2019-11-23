fe6ca56a5465aa523d3f6e90a6be83b2d8e8e7c0
Grant Phelps <gbphelps2@gmail.com>
2019-11-23 13:30:53 -0500

    Add the previous color to the arguments provided by the subscription

--- javascripts/ColorObject.js
+++ javascripts/ColorObject.js
@@ -5,6 +5,16 @@ import hsvFromRGB from './colorMethods/hsvFromRGB';
 import hsluvFromRGB from './colorMethods/hsluvFromRGB';
 import rgbFromHSLUV from './colorMethods/rgbFromHSLUV';
 
+function deepDup(obj){
+    const newObj = {};
+    Object.keys(obj).forEach(k => {
+        newObj[k] = (typeof obj[k] === 'object') ?
+        deepDup(obj[k]) :
+        obj[k]
+    })
+    return newObj;
+}
+
 export class Color {
     constructor(){
         this.color = {
@@ -41,43 +51,47 @@ export class Color {
 	}
 	
 	setRGB(rgb){
+        const prev = deepDup(this.color);
         Object.assign(this.color.rgb, rgb);
 
         this.color.hsv = hsvFromRGB(this.color.rgb);
         this.color.hsl = hslFromRGB(this.color.rgb);
         this.color.hsluv = hsluvFromRGB(this.color.rgb);
 
-		this.subscriptions.forEach(subscription => subscription(this.color));
+		this.subscriptions.forEach(subscription => subscription(this.color, prev));
 	}
 	
 	setHSV(hsvPartial){
+        const prev = deepDup(this.color);
         Object.assign(this.color.hsv, hsvPartial);
 
         this.color.rgb = rgbFromHSV(this.color.hsv);
         this.color.hsl = hslFromRGB(this.color.rgb);
         this.color.hsluv = hsluvFromRGB(this.color.rgb);
 
-		this.subscriptions.forEach(subscription => subscription(this.color));
+		this.subscriptions.forEach(subscription => subscription(this.color, prev));
     }
     
     setHSL(hslPartial){
+        const prev = deepDup(this.color);
         Object.assign(this.color.hsl, hslPartial);
 
         this.color.rgb = rgbFromHSL(this.color.hsl);
         this.color.hsv = hsvFromRGB(this.color.rgb);
         this.color.hsluv = hsluvFromRGB(this.color.rgb);
 
-        this.subscriptions.forEach(subscription => subscription(this.color));
+        this.subscriptions.forEach(subscription => subscription(this.color, prev));
     }
 
     setHSLUV(hsluvPartial){
+        const prev = deepDup(this.color);
         Object.assign(this.color.hsluv, hsluvPartial);
 
         this.color.rgb = rgbFromHSLUV(this.color.hsluv);
         this.color.hsv = hsvFromRGB(this.color.rgb);
         this.color.hsl = hslFromRGB(this.color.rgb);
 
-        this.subscriptions.forEach(subscription => subscription(this.color));
+        this.subscriptions.forEach(subscription => subscription(this.color, prev));
     }
 }
 
