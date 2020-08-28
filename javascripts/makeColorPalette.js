f029edf08cd11ac7d2d95feaa6d5ff55d3f20092
Grant Phelps <gbphelps2@gmail.com>
2020-08-27 20:01:22 -0400

    isDark

--- javascripts/makeColorPalette.js
+++ javascripts/makeColorPalette.js
@@ -1,5 +1,6 @@
 import mainColor from './ColorObject';
 import namedColors from './namedColors';
+import hsvFromRGB from './colorMethods/hsvFromRGB';
 
 
 
@@ -18,17 +19,35 @@ function hexFromRGB(rgb) {
 
 const channels = ['red','green','blue'];
 
+
+const c1Lookup = {};
+
+function colorNameHSV(name){
+    if (c1Lookup[name]){
+        return c1Lookup[name];
+    }
+    c1Lookup[name] = hsvFromRGB({
+        red: parseInt(namedColors[name].slice(1, 3),16),
+        green: parseInt(namedColors[name].slice(3, 5),16),
+        blue: parseInt(namedColors[name].slice(5, 7),16)
+    });
+    return c1Lookup[name];
+}
+
 function closestNamedColor(color){
     let best = null;
-    const denom = 255 * Math.sqrt(3);
-
     Object.keys(namedColors).forEach(k => {
-        let squareSum = 0;
-        for (let i=0; i<3; i++){
-            const channel = parseInt(namedColors[k].slice(i*2+1, i*2+3),16);
-            const channel2 = color[channels[i]];
-            squareSum += (channel - channel2)**2;
-        }
+        const c1 = colorNameHSV(k);
+    
+        const c2 = hsvFromRGB(color);
+    
+        const hmin = Math.min(c1.hue, c2.hue);
+        const hmax = Math.max(c1.hue, c2.hue);
+        let hdiff = Math.min(hmax-hmin, hmin+360-hmax);
+        hdiff = isNaN(hdiff) ? 0 : hdiff;
+    
+        const squareSum = (hdiff/360)**2*2 + ((c1.saturation - c2.saturation)/100)**2 + ((c1.value - c2.value)/100)**2*2;
+
         if (!best || best.distance > squareSum){
             best = {
                 color: k,
@@ -38,21 +57,39 @@ function closestNamedColor(color){
     })
     return {
         color: best.color,
-        distance: (1 - Math.sqrt(best.distance)/denom)*100,
+        distance: ((1 - Math.sqrt(best.distance/6))*100).toFixed(),
     };
 }
 
+
+
+function mix(c1, c2, t){
+    return {
+        red: c1.red + (c2.red - c1.red)*t,
+        green: c1.green + (c2.green - c1.green)*t,
+        blue: c1.blue + (c2.blue - c1.blue)*t,
+    }
+}
+
+
+function isDark(rgb) {
+    return .2126*rgb.red + .7152*rgb.green + .0722*rgb.blue < .68*255;
+}
 
 export default function makeColorPalette({target}) {
     const currentColor = document.createElement('div');
     currentColor.classList.add('current-color');
     target.appendChild(currentColor);
     mainColor.subscribe(COLOR => {
+       const secondaryRGB = mix({red: 255, green: 255, blue: 255}, COLOR.rgb, .3);
+
        const hexColor = hexFromRGB(COLOR.rgb);
        const closest = closestNamedColor(COLOR.rgb);
        currentColor.style.background = hexColor;
-       currentColor.innerHTML = hexColor;
-       currentColor.innerHTML = `<div>${closest.color.toUpperCase()}</div><div>${closest.distance.toFixed()}% match</div>`;
+       currentColor.classList.add(isDark(COLOR.rgb) ? 'dark' : 'light');
+       currentColor.classList.remove(isDark(COLOR.rgb) ? 'light' : 'dark');
 
+       currentColor.innerHTML = hexColor;
+       currentColor.innerHTML = `<div>${closest.color.toUpperCase()}</div><div>${closest.distance}% match</div>`;
     })
 }
