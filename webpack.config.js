1173a4f72155ac4e3788223c8896da9caccc248e
Grant Phelps <gbphelps2@gmail.com>
2021-02-01 09:37:35 -0500

    Lint, remember that layout is breaking a little due to flex at small sizes, remove flex entirely in favor of js resizing

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'javascripts', 'index.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist')
-    },
-    module: {
-        rules: [{
-            test: /\.glsl$/,
-            use: 'raw-loader',
-        }]
-    }
-}
+  entry: path.resolve(__dirname, 'javascripts', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.glsl$/,
+      use: 'raw-loader',
+    }],
+  },
+};
